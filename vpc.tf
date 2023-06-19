196cab3e8bf6d5fc7a61ea43c4f209de0516f62b
Sou Yonamine <sasisusesou24@gmail.com>
2023-06-19 17:09:33 +0900

    Add images

--- vpc.tf
+++ vpc.tf
@@ -84,7 +84,7 @@ resource "aws_subnet" "private4" {
 ######################################################################################
 # Route table
 ######################################################################################
-#Route table
+#Route table Public
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -111,144 +111,170 @@ resource "aws_route_table_association" "public2" {
   route_table_id = aws_route_table.public.id
 }
 
-#resource "aws_subnet" "private" {
-
-#  count             = length(var.private_subnet_cidr)
-#  vpc_id            = aws_vpc.main.id
-#  cidr_block        = var.private_subnet_cidr[count.index]
-#  availability_zone       = count.index < 2 ? var.availability_zones[0] : var.availability_zones[1]
-#  map_public_ip_on_launch = false
-#
-#  tags = {
-#    Name = "${var.tag_prefix}private-subnet-${count.index + 1}"
-#  }
-#}
-#
-#
-#
 ######################################################################################
 ## network acl 
 ######################################################################################
-#resource "aws_network_acl_association" "public" {
-#  count          = length(var.public_subnet_cidr)
-#  subnet_id      = aws_subnet.public[count.index].id 
-#  network_acl_id = aws_network_acl.public.id
-#}
-#
-#resource "aws_network_acl" "private1" {
-#  vpc_id = aws_vpc.main.id
-#
-#  ingress {
-#    protocol   = "-1"
-#    rule_no    = 100
-#    action     = "allow"
-#    cidr_block = "10.0.0.0/24"  
-#    from_port  = 0
-#    to_port    = 0
-#  }
-#
-#  ingress {
-#    protocol   = "-1"
-#    rule_no    = 100
-#    action     = "allow"
-#    cidr_block = "10.0.2.0/24"  
-#    from_port  = 0
-#    to_port    = 0
-#  }
-#
-#  egress {
-#    protocol   = "-1"
-#    rule_no    = 100
-#    action     = "allow"
-#    cidr_block = "10.0.0.0/24"  
-#    from_port  = 0
-#    to_port    = 0
-#  }
-#
-#  egress {
-#    protocol   = "-1"
-#    rule_no    = 100
-#    action     = "allow"
-#    cidr_block = "10.0.2.0/24"  
-#    from_port  = 0
-#    to_port    = 0
-#  }
-#
-#  # デフォルトルールは全ての通信を拒否
-#  egress {
-#    protocol   = "-1"
-#    rule_no    = 500
-#    action     = "deny"
-#    cidr_block = "0.0.0.0/0"
-#    from_port  = 0
-#    to_port    = 0
-#  }
-#
-#  ingress {
-#    protocol   = "-1"
-#    rule_no    = 500
-#    action     = "deny"
-#    cidr_block = "0.0.0.0/0"
-#    from_port  = 0
-#    to_port    = 0
-#  }
-#
-#  tags = {
-#    Name = "${var.tag_prefix}private1-nacl"
-#  }
-#}
-#
-#resource "aws_network_acl_association" "private1" {
-#  subnet_id      = aws_subnet.private1.id
-#  network_acl_id = aws_network_acl.private1.id
-#}
-#
-#resource "aws_network_acl" "private2" {
-#  vpc_id = aws_vpc.main.id
-#
-#  ingress {
-#    protocol   = "-1"
-#    rule_no    = 100
-#    action     = "allow"
-#    cidr_block = "10.0.1.0/24"  # 入力元はprivate_subnet1
-#    from_port  = 0
-#    to_port    = 0
-#  }
-#
-#  egress {
-#    protocol   = "-1"
-#    rule_no    = 200
-#    action     = "allow"
-#    cidr_block = "10.0.1.0/24"  # 出力先はprivate_subnet1
-#    from_port  = 0
-#    to_port    = 0
-#  }
-#
-#  # デフォルトルールは全ての通信を拒否
-#  egress {
-#    protocol   = "-1"
-#    rule_no    = 300
-#    action     = "deny"
-#    cidr_block = "0.0.0.0/0"
-#    from_port  = 0
-#    to_port    = 0
-#  }
-#
-#  ingress {
-#    protocol   = "-1"
-#    rule_no    = 300
-#    action     = "deny"
-#    cidr_block = "0.0.0.0/0"
-#    from_port  = 0
-#    to_port    = 0
-#  }
-#
-#  tags = {
-#    Name = "${var.tag_prefix}private2-nacl"
-#  }
-#}
-#
-#resource "aws_network_acl_association" "private2" {
-#  subnet_id      = aws_subnet.private2.id
-#  network_acl_id = aws_network_acl.private2.id
-#}
+# private1
+resource "aws_network_acl" "private1" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.0.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 101
+    action     = "allow"
+    cidr_block = "10.0.2.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.0.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 101
+    action     = "allow"
+    cidr_block = "10.0.2.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${var.tag_prefix}private1-nacl"
+  }
+}
+
+resource "aws_network_acl_association" "private1" {
+  subnet_id = aws_subnet.private1.id
+  network_acl_id = aws_network_acl.private1.id
+}
+
+#private2
+resource "aws_network_acl" "private2" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.1.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.1.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${var.tag_prefix}private2-nacl"
+  }
+}
+
+resource "aws_network_acl_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  network_acl_id = aws_network_acl.private2.id
+}
+
+# private3
+resource "aws_network_acl" "private3" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.10.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 101
+    action     = "allow"
+    cidr_block = "10.0.12.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.10.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 101
+    action     = "allow"
+    cidr_block = "10.0.12.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${var.tag_prefix}private3-nacl"
+  }
+}
+
+resource "aws_network_acl_association" "private3" {
+  subnet_id = aws_subnet.private3.id
+  network_acl_id = aws_network_acl.private3.id
+}
+
+
+#private4
+resource "aws_network_acl" "private4" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.11.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.11.0/24"  
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${var.tag_prefix}private4-nacl"
+  }
+}
+
+resource "aws_network_acl_association" "private4" {
+  subnet_id      = aws_subnet.private4.id
+  network_acl_id = aws_network_acl.private4.id
+}
