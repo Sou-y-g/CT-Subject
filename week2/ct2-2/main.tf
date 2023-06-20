92a4d8cc2e2609b2c72522db155b6adb50303d58
Sou Yonamine <sasisusesou24@gmail.com>
2023-06-20 09:18:34 +0900

    Add image

--- week2/ct2-2/main.tf
+++ week2/ct2-2/main.tf
@@ -3,8 +3,6 @@
 ######################################################################################
 resource "aws_vpc" "vpc1" {
   cidr_block = var.vpc1_cidr
-  #enable_dns_hostnames = true
-  #enable_dns_support = true
 
   tags = {
     Name = "${var.tag_prefix}vpc1"
@@ -24,8 +22,6 @@ resource "aws_subnet" "private1" {
 
 resource "aws_vpc" "vpc2" {
   cidr_block = var.vpc2_cidr
-  #enable_dns_hostnames = true
-  #enable_dns_support = true
 
   tags = {
     Name = "${var.tag_prefix}vpc2"
